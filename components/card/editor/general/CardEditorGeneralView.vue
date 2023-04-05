59ace384ad6cc227ad703626623218eb1f48e1eb
DanikingRD <danikingrd@gmail.com>
2023-04-05 19:24:25 -0400

    Improve card editor text-fields

--- components/card/editor/general/CardEditorGeneralView.vue
+++ components/card/editor/general/CardEditorGeneralView.vue
@@ -1,42 +1,47 @@
 <template>
   <DashNote title="Label this card" icon="mdi-account-tag-outline" />
-  <TextField
+  <!--I want the title to be centered aligned-->
+
+  <!-- <TextField
+    class="centered-input"
     label="Card Title"
-    style="text-align: center"
     hint="Enter the title for this card"
     v-model="card.label"
     density="comfortable"
-    variant="filled"
-  />
+    variant="underlined"
+  /> -->
+  <v-text-field
+    class="centered-input"
+    label="Card Title"
+    hint="Enter the title for this card"
+    v-model="card.label"
+    density="comfortable"
+    variant="underlined"
+    style="text-align: center; max-width: 360px; margin: 0 auto"
+  >
+  </v-text-field>
   <DashNote title="Add your details" icon="mdi-account-outline" />
   <v-row class="mt-0" style="height: 90px">
     <v-col>
       <TextField
+        class="rounded-field"
         label="First Name"
-        density="comfortable"
+        density="compact"
         v-model="card.firstName"
       />
     </v-col>
     <v-col>
-      <TextField
-        label="Last Name"
-        density="comfortable"
-        v-model="card.lastName"
-      />
+      <TextField label="Last Name" density="compact" v-model="card.lastName" />
     </v-col>
   </v-row>
   <v-row class="mt-0">
     <v-col>
-      <TextField
-        label="Job Title"
-        density="comfortable"
-        v-model="card.jobTitle"
-      />
+      <TextField label="Job Title" density="compact" v-model="card.jobTitle" />
     </v-col>
     <v-col>
       <TextField
         label="Company Name"
-        density="comfortable"
+        density="compact"
         v-model="card.company"
       />
     </v-col>
@@ -47,3 +52,20 @@
 import { useCardEditorStore } from "~~/stores/card-editor.store";
 const { card } = storeToRefs(useCardEditorStore());
 </script>
+<style lang="scss">
+.rounded-field {
+  border-radius: 24px !important;
+}
+.centered-input input {
+  text-align: center;
+}
+
+.v-text-field.centered-input .v-label {
+  left: 42% !important;
+
+  &.v-label--active {
+    transform: translateY(-18px) scale(0.75) translateX(-50%);
+    transform: translateY(-80%);
+  }
+}
+</style>
